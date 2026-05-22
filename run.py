d36a536b5cd76fcc0c238441fb33bac89ee1d71e
crisc <62117443+crischai1@users.noreply.github.com>
2026-05-22 07:42:08 -0700

    Change host to localhost

--- run.py
+++ run.py
@@ -10,4 +10,4 @@ app, socketio = create_app()
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "5001"))
     debug = os.environ.get("FLASK_DEBUG", "0") == "1"
-    socketio.run(app, host="0.0.0.0", port=port, debug=debug)
+    socketio.run(app, host="127.0.0.1", port=port, debug=debug)
